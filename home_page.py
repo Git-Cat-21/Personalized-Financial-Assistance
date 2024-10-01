a6365a20505a03f7d0b473a48833a0fd2681a1ad
mahika34 <mahi.datta20@gmail.com>
2024-10-01 19:18:46 +0530

    Updated

--- home_page.py
+++ home_page.py
@@ -1,14 +1,20 @@
 from tkinter import *
 
 root=Tk()
-frame=Frame(root)
+root.title('Title')
+root.geometry("500x500")
+frame=Frame(root,width=500,height=500)
 frame.pack()
+title
+
 signup = Button(frame, text='Signup')
 signup.pack()
 login = Button(frame, text='Login')
 login.pack()
 about_us = Button(frame, text='About Us')
 about_us.pack()
-contact_us = Button(frame, text='Contact Us')#below scroll
+contact_us = Button(frame, text='Contact Us')
 contact_us.pack()
+# about_us_text=Text(frame,text='About Us')
+# about_us_text.pack()
 root.mainloop()

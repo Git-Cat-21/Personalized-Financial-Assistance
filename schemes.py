fa2975e952bef279e7a2b722894e648924c81b4e
Git-Cat-21 <khushmish21@gmail.com>
2024-10-01 22:44:29 +0530

    table scheme changed

--- schemes.py
+++ schemes.py
@@ -17,7 +17,7 @@ root_scheme.title("Schemes")
 root_scheme.geometry("800x800")
 
 tree=ttk.Treeview(root_scheme)
-tree["columns"]=("Scheme_Name","Rate of Interest","Duration")
+tree["columns"]=("Scheme_ID","Scheme_Name","Rate of Interest","Duration_In_Years")
 tree['show']='headings'
 tree_style=ttk.Style(root_scheme)
 
@@ -25,18 +25,20 @@ tree_style.theme_use("clam")
 tree_style.configure(".",font=("Helvetica",11))
 tree_style.configure("Treeview.Heading",foreground='red',font=("Helvetica",11,'bold'))
 
+tree.column("Scheme_ID",width=150,minwidth=50,anchor=CENTER)
 tree.column("Scheme_Name",width=200,minwidth=50,anchor=CENTER)
 tree.column("Rate of Interest",width=150,minwidth=50,anchor=CENTER)
-tree.column("Duration",width=150,minwidth=50,anchor=CENTER)
+tree.column("Duration_In_Years",width=150,minwidth=50,anchor=CENTER)
 
+tree.heading("Scheme_ID",text="Scheme_ID",anchor=CENTER)
 tree.heading("Scheme_Name",text="Scheme_Name",anchor=CENTER)
 tree.heading("Rate of Interest",text="Rate of Interest",anchor=CENTER)
-tree.heading("Duration",text="Duration",anchor=CENTER)
+tree.heading("Duration_In_Years",text="Duration_In_Years",anchor=CENTER)
 
-tree.insert('',0,text="",values=("Scheme Name","Rate of Interest","Duration"))
+tree.insert('',0,text="",values=("Scheme ID","Scheme Name","Rate of Interest","Duration in years"))
 i=0
 for row in cursor:
-    tree.insert('',i,text="",values=(row[0],row[1],row[2]))
+    tree.insert('',i,text="",values=(row[0],row[1],row[2],row[3]))
     i=i+1
 tree.pack()
 root_scheme.mainloop()

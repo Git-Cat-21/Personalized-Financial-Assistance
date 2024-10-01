8dffb573e4de02f566aec655607083d51d85e87a
Git-Cat-21 <khushmish21@gmail.com>
2024-10-01 10:24:35 +0530

    table schema updated

--- all_tables.sql
+++ all_tables.sql
@@ -3,26 +3,21 @@ CREATE DATABASE IF NOT EXISTS pfa_orange;
 USE pfa_orange;
 
 CREATE TABLE schemes(
-    Scheme_Name VARCHAR(30) PRIMARY KEY,
+    Scheme_ID INT PRIMARY KEY,
+    Scheme_Name VARCHAR(40),
     Interest_Rate FLOAT NOT NULL,
-    Duration VARCHAR(10) NOT NULL
+    Duration_In_Years FLOAT NOT NULL
 );
-ALTER TABLE schemes
-MODIFY COLUMN Scheme_Name VARCHAR(40)
 
-
-DELETE FROM schemes;
-INSERT INTO schemes (Scheme_Name,Interest_Rate, Duration) VALUES
-('SecureBank FD', 6.5, '1 year'),
-('GoldTrust Growth FD', 7.0, '2 years'),
-('SafeInvest Future Plan', 7.5, '5 years'),
-('TrustCapital Secure FD', 6.8, '3 years'),
-('PrimeBank Senior Citizen FD', 8.0, '1.5 years'),
-('GrowthBank Fixed Plan', 8.2, '10 years'),
-('HorizonFinance Flexi FD', 5.0, '6 months'),
-('FirstChoice Wealth FD', 7.2, '4 years'),
-('ValueSafe High Return FD', 6.75, '1 year'),
-('CapitalMax Booster FD', 7.1, '3 years');
-
-
+INSERT INTO schemes (Scheme_ID,Scheme_Name,Interest_Rate, Duration_In_Years) VALUES
+(1,'SecureBank FD', 6.5, 1),
+(2,'GoldTrust Growth FD', 7.0, 2),
+(3,'SafeInvest Future Plan', 7.5, 5),
+(4,'TrustCapital Secure FD', 6.8, 3),
+(5,'PrimeBank Senior Citizen FD', 8.0, 1.5),
+(6,'GrowthBank Fixed Plan', 8.2, 10),
+(7,'HorizonFinance Flexi FD', 5.0, 6),
+(8,'FirstChoice Wealth FD', 7.2, 0.5),
+(9,'ValueSafe High Return FD', 6.75, 1),
+(10,'CapitalMax Booster FD', 7.1, 3);
 SELECT * FROM schemes;

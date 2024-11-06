e0f1a11784b8c7be2dde90a73ee4c0bc0415783e
mahi.datta20@gmail.com <mahi.datta20@gmail.com>
2024-11-06 12:22:29 +0530

    Final changes done!

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def initialize_session():
     if 'userid' not in session:
         session['userid'] = None
 
-
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -25,7 +24,7 @@ cursor = db.cursor()
 
 def login_required(f):
     def decorated_function(*args, **kwargs):
-        if 'username' not in session:
+        if session.get('username') is None or session.get('userid') is None:
             flash("Please log in to access this page.", "warning")
             return redirect(url_for('login'))
         return f(*args, **kwargs)
@@ -34,44 +33,42 @@ def login_required(f):
 
 @app.route("/")
 def index():
-    userid = session['userid']
+    userid = session.get('userid')
     cursor.execute('SELECT * FROM user_details WHERE User_ID = %s', (userid,))
     query = cursor.fetchall()
-    return render_template('homepage.html', query = query)
+    return render_template('homepage.html', query=query)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
-    if request.method == 'POST':
-        global username
+    elif request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         cursor = db.cursor()
         cursor.execute('SELECT User_ID, Pwd, User_name FROM user_details WHERE User_ID = %s', (username,))
-        global user
         user = cursor.fetchone()
         if user and user[1] == password:  
             session['username'] = user[2]  
             session['userid'] = user[0]
             flash("Login successful!", "success")
-            return redirect('/')
+            return redirect(url_for('index'))
         else:
             flash("Invalid username or password.", "danger")
             return redirect(url_for('login'))
 
 @app.route("/logout")
 def logout():
-    session.pop('username', None) 
-    session.pop('userid', None)  
+    session.pop('username', None)
+    session.pop('userid', None)
     flash("You have been logged out.", "success")
-    print(session)
-    return redirect('/')     
+    return redirect(url_for('index'))
 
 @app.route("/schemes")
 def schemes():
     return redirect("http://127.0.0.1:3000/schemes")
 
+   
 @app.route("/forgot_pwd", methods=['GET', 'POST'])
 def forgot_pwd():
     if request.method == 'GET':
@@ -109,36 +106,50 @@ def signup():
         else:
             return redirect(url_for('signup'))
         return redirect(url_for('login'))
-
+    
 @app.route("/savings", methods=['GET', 'POST'])
 @login_required
 def savings():
     if request.method == 'GET':
         return render_template('savings.html')
-    if request.method == 'POST':
+    elif request.method == 'POST':
         trans_id = request.form['trans_id']
         user_id = request.form['user_id']
         scheme_id = request.form['scheme_id']
         amount = request.form['amount']
 
-        print(trans_id)
-        cursor = db.cursor()
-        query = f"SELECT user_details.Mob, account_details.acc_no, account_details.pan FROM user_details JOIN account_details ON user_details.User_ID = account_details.user_id WHERE user_details.User_ID = {user_id}"
-        cursor.execute(query)
+        cursor.execute(f'''
+            SELECT user_details.Mob, account_details.acc_no, account_details.pan 
+            FROM user_details 
+            JOIN account_details 
+            ON user_details.User_ID = account_details.user_id 
+            WHERE user_details.User_ID = %s
+        ''', (user_id,))
         result = cursor.fetchone()
-        print(result)
+        
         if result:
-            cursor.execute('''SELECT calc_int_amt(%s, %s)''', (amount, scheme_id))
+            cursor.execute('SELECT calc_int_amt(%s, %s)', (amount, scheme_id))
             mat_amt = cursor.fetchone()[0]
             mat_date = maturity_date(int(scheme_id))
-            print(mat_date)
-            cursor.execute('''INSERT INTO savings_details (user_id_savings, account_number, mobile_number, Scheme_ID, amount, pan, Maturity_Amount, invested_date, Maturity_Date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ''', (user_id, result[1], result[0], scheme_id, amount, result[2], mat_amt, mat_date[0], mat_date[1]))
+            
+            cursor.execute('''
+                INSERT INTO savings_details 
+                (user_id_savings, account_number, mobile_number, Scheme_ID, amount, pan, Maturity_Amount, invested_date, Maturity_Date) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ''', (user_id, result[1], result[0], scheme_id, amount, result[2], mat_amt, mat_date[0], mat_date[1]))
+            
             db.commit()
-            cursor = db.cursor()
-            cursor.execute('''INSERT INTO transactions(Transaction_ID, User_ID, Debit_Amount, Debit_Date,Credit_Amount,Credit_Date) VALUES (%s, %s, %s, %s, %s, %s)''', (trans_id, user_id, amount, mat_date[0],mat_amt,mat_date[1]))
+            
+            cursor.execute('''
+                INSERT INTO transactions
+                (Transaction_ID, User_ID, Debit_Amount, Debit_Date, Credit_Amount, Credit_Date) 
+                VALUES (%s, %s, %s, %s, %s, %s)
+            ''', (trans_id, user_id, amount, mat_date[0], mat_amt, mat_date[1]))
+            
             db.commit()
             return redirect(url_for('schemes'))
         else:
+            flash("User details not found. Please try again.", "warning")
             return redirect(url_for("savings"))
 
 if __name__ == "__main__":
